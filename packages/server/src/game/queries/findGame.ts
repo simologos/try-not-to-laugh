fc5554bf3726fc2e18fac04b7a805880c722ab6c
Simon Egli <simon.egli@bucher-suter.com>
2020-12-19 15:30:10 +0100

    Add list of user entities to the get game query

--- packages/server/src/game/queries/findGame.ts
+++ packages/server/src/game/queries/findGame.ts
@@ -1,11 +1,21 @@
 import IGame from "@tntl/definition/src/game/IGame";
+import IExtendedPlayerList from "@tntl/definition/src/game/IExtendedPlayerList";
 import IError from "@tntl/definition/src/generic/IError";
 import IResponse from "@tntl/definition/src/generic/IResponse";
 import { gameModel } from "../repository/model";
+import { userModel } from "../../user/repository/model";
+import { Types } from "mongoose";
 
-export const getGameById = async (id: string): Promise<IResponse & (IGame | IError)> => {
+export const getGameById = async (id: string)
+: Promise<IResponse & (IGame & IExtendedPlayerList | IError)> => {
+
   try {
     const game = await gameModel.findById(id).exec();
+    const users = await userModel.find({
+      '_id': {
+        $in: game.players.map(e => Types.ObjectId(e))
+      }
+    }).exec();
 
     return {
       success: true,
@@ -13,9 +23,19 @@ export const getGameById = async (id: string): Promise<IResponse & (IGame | IErr
       currentRound: game.currentRound,
       players: game.players,
       playlist: game.playlist,
-      state: game.state
+      state: game.state,
+      users: users.map(e => ({
+        id: e.id,
+        displayName: e.displayName,
+        firstName: e.firstName,
+        lastName: e.lastName,
+        avatar: e.avatar,
+        googleId: e.googleId,
+        isValidated: e.isValidated,
+        score: e.score
+      }))
     };
-  } catch(e){
+  } catch (e) {
     return {
       success: false,
       error: e.message
